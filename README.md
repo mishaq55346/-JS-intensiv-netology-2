03eff26ed5cf76c6ceba3eac758e04c58db128db
Mikhail <misha.kolmykovqq1@yandex.ru>
2021-10-29 23:46:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# -JS-intensiv-netology-2
+ 
---
+++ src/components/AppModal.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="exampleModal"
+         tabindex="-1"
+         aria-labelledby="exampleModalLabel"
+         class="modal fade"
+         style="display: none;" aria-hidden="true"
+    >
+        <div class="modal-dialog">
+
+          <!--<pre>{{character}}</pre>-->
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{character.name}}</h5>
+                    <button type="button"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                            class="btn-close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <img :src="character.thumbnail"
+                         :alt="character.name"
+                         style="max-width: 100%;"
+                    >
+                    <div>
+                        <p class="text-muted">{{character.modified}}</p>
+                        <h5>Описание:</h5>
+                        <p>
+                            {{character.description}}
+                        </p>
+                        <h5>Комиксы:</h5>
+                        <ul>
+                            <li v-for="elComics in character.comics"
+                            :key="elComics.name"
+                            > {{elComics.name}}</li>
+                            <li> Adventures of Spider-Man (1996) #2</li>
+                            <li> Adventures of Spider-Man (1996) #3</li>
+                        </ul>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button"
+                                data-bs-dismiss="modal"
+                                class="btn btn-secondary  btn-sm"
+                        >
+                            Закрыть
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppModal",
+        props: ["character"],
+    }
+</script>
+
+<style scoped>
+
+</style>
